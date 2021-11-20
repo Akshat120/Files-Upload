b3ba5e9585372081da606f904e9c40fbfb17a42f
Zepurts <akshat.dhiman15@gmail.com>
2021-11-20 11:45:14 +0530

    Remake Gridfs.

--- _GridFs_/app.js
+++ _GridFs_/app.js
@@ -1,8 +1,31 @@
+require('dotenv').config();
+
 const express = require('express');
+const bodyParser = require('body-parser');
 const app = express();
+const path = require('path');
+const crypto = require('crypto');
+const mongoose = require('mongoose');
+const multer = require('multer');
+const GridFsStorage = require('multer-gridfs-storage');
+const Grid = require('gridfs-stream');
+const methodOverride = require('method-override');
+ 
+// Middleware
+app.set('view engine','ejs');
+app.use(bodyParser.json());
+app.use(methodOverride('_method'))
+
+const mongoURI = process.env.CONNECTIONSTRING;
+
 
 app.get('/',(req,res)=>{
-    res.send('GridFs');
+    res.render('index') 
 });
 
-module.exports = app;
+
+app.listen(3000,()=>{
+    console.log("Server Started.");
+})
+
+
